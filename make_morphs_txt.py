906272471afb158a95c2b6e65155d849429a08a6
dong <459188369@qq.com>
2024-10-11 08:03:13 +0800

    1

--- make_morphs_txt.py
+++ make_morphs_txt.py
@@ -1,4 +1,62 @@
 import numpy as np
+from scipy.spatial.transform import Rotation as RTT
+
+
+def deal_eye_pose(eye_pose):
+    up_down_range = 35
+    left_right_range = 55
+
+    eye_coeffs = np.zeros([eye_pose.shape[0], 8])
+    smpl_euler = RTT.from_rotvec(eye_pose[:, -6:-3]).as_euler('xyz', degrees=True)
+    smpl_euler[:, 0] /= up_down_range
+    smpl_euler[:, 1] /= left_right_range
+    smpl_euler[:, 0] = np.clip(smpl_euler[:, 0], -1, 1)
+    smpl_euler[:, 1] = np.clip(smpl_euler[:, 1], -1, 1)
+    for i in range(smpl_euler.shape[0]):
+        if(smpl_euler[i, 0]>=0 and smpl_euler[i, 1]>=0):
+            eye_coeffs[i, 0] = smpl_euler[i, 0]
+            eye_coeffs[i, 1] = smpl_euler[i, 1]
+        elif(smpl_euler[i, 0]>=0 and smpl_euler[i, 1]<0):
+            eye_coeffs[i, 0] = smpl_euler[i, 0]
+            eye_coeffs[i, 3] = -smpl_euler[i, 1]
+        elif(smpl_euler[i, 0]<0 and smpl_euler[i, 1]>=0):
+            eye_coeffs[i, 2] = -smpl_euler[i, 0]
+            eye_coeffs[i, 1] = smpl_euler[i, 1]
+        elif(smpl_euler[i, 0]<0 and smpl_euler[i, 1]<0):
+            eye_coeffs[i, 2] = -smpl_euler[i, 0]
+            eye_coeffs[i, 3] = -smpl_euler[i, 1]
+
+    smpl_euler = RTT.from_rotvec(eye_pose[:, -3:]).as_euler('xyz', degrees=True)
+    smpl_euler[:, 0] /= up_down_range
+    smpl_euler[:, 1] /= left_right_range
+    smpl_euler[:, 0] = np.clip(smpl_euler[:, 0], -1, 1)
+    smpl_euler[:, 1] = np.clip(smpl_euler[:, 1], -1, 1)
+    for i in range(smpl_euler.shape[0]):
+        if(smpl_euler[i, 0]>=0 and smpl_euler[i, 1]>=0):
+            eye_coeffs[i, 4] = smpl_euler[i, 0]
+            eye_coeffs[i, 5] = smpl_euler[i, 1]
+        elif(smpl_euler[i, 0]>=0 and smpl_euler[i, 1]<0):
+            eye_coeffs[i, 4] = smpl_euler[i, 0]
+            eye_coeffs[i, 7] = -smpl_euler[i, 1]
+        elif(smpl_euler[i, 0]<0 and smpl_euler[i, 1]>=0):
+            eye_coeffs[i, 6] = -smpl_euler[i, 0]
+            eye_coeffs[i, 5] = smpl_euler[i, 1]
+        elif(smpl_euler[i, 0]<0 and smpl_euler[i, 1]<0):
+            eye_coeffs[i, 6] = -smpl_euler[i, 0]
+            eye_coeffs[i, 7] = -smpl_euler[i, 1]
+
+    avg_weight = 0.7
+    eye_coeffs_ret = np.zeros_like(eye_coeffs)
+    eye_coeffs_ret[:, 0] = (eye_coeffs[:, 0] + eye_coeffs[:, 4])/2 * avg_weight + eye_coeffs[:, 0] * (1 - avg_weight)
+    eye_coeffs_ret[:, 4] = (eye_coeffs[:, 0] + eye_coeffs[:, 4])/2 * avg_weight + eye_coeffs[:, 4] * (1 - avg_weight)
+    eye_coeffs_ret[:, 1] = (eye_coeffs[:, 1] + eye_coeffs[:, 5])/2 * avg_weight + eye_coeffs[:, 1] * (1 - avg_weight)
+    eye_coeffs_ret[:, 5] = (eye_coeffs[:, 1] + eye_coeffs[:, 5])/2 * avg_weight + eye_coeffs[:, 5] * (1 - avg_weight)
+    eye_coeffs_ret[:, 2] = (eye_coeffs[:, 2] + eye_coeffs[:, 6])/2 * avg_weight + eye_coeffs[:, 2] * (1 - avg_weight)
+    eye_coeffs_ret[:, 6] = (eye_coeffs[:, 2] + eye_coeffs[:, 6])/2 * avg_weight + eye_coeffs[:, 6] * (1 - avg_weight)
+    eye_coeffs_ret[:, 3] = (eye_coeffs[:, 3] + eye_coeffs[:, 7])/2 * avg_weight + eye_coeffs[:, 3] * (1 - avg_weight)
+    eye_coeffs_ret[:, 7] = (eye_coeffs[:, 3] + eye_coeffs[:, 7])/2 * avg_weight + eye_coeffs[:, 7] * (1 - avg_weight)
+    return eye_coeffs_ret
+
 
 if(__name__=='__main__'):
     neutral = np.zeros([35])
@@ -66,11 +124,17 @@ if(__name__=='__main__'):
     # 190 * 25 (viewport) = 4750 images
     emo_morphTargets = np.concatenate([emo_morphTargets, np.eye(35), np.zeros([1, 35])], axis=0)
 
-    eye_morphTargets = np.zeros([4, 8])
-    eye_morphTargets[0, 0] = eye_morphTargets[0, 4] = 0.5
-    eye_morphTargets[1, 1] = eye_morphTargets[1, 5] = 0.5
-    eye_morphTargets[2, 2] = eye_morphTargets[2, 6] = 0.5
-    eye_morphTargets[3, 3] = eye_morphTargets[3, 7] = 0.5
+    eye_pose = np.array([[np.pi/6, 0, 0, np.pi/6, 0, 0], 
+                         [0, np.pi/6, 0, 0, np.pi/6, 0], 
+                         [-np.pi/6, 0, 0, -np.pi/6, 0, 0], 
+                         [0, -np.pi/6, 0, 0, -np.pi/6, 0]])
+    eye_morphTargets = deal_eye_pose(eye_pose)
+
+    # eye_morphTargets = np.zeros([4, 8])
+    # eye_morphTargets[0, 0] = eye_morphTargets[0, 4] = 0.5
+    # eye_morphTargets[1, 1] = eye_morphTargets[1, 5] = 0.5
+    # eye_morphTargets[2, 2] = eye_morphTargets[2, 6] = 0.5
+    # eye_morphTargets[3, 3] = eye_morphTargets[3, 7] = 0.5
 
     eye_morphTargets = np.pad(eye_morphTargets, [[emo_morphTargets.shape[0], 0], [0, 0]], mode='constant', constant_values=0)
     emo_morphTargets = np.pad(emo_morphTargets, [[0, 4], [0, 0]], mode='constant', constant_values=0)
